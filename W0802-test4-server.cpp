a5f50ed2412cd0ff0c12dd5acf854591503f0226
LovingYoung <ly950710@gmail.com>
2016-04-25 20:05:43 +0800

    Update W0802-test4-server.cpp

--- W0802-test4-server.cpp
+++ W0802-test4-server.cpp
@@ -1,157 +1,35 @@
+#include <iostream>
 #include <zconf.h>
 #include <sys/param.h>
 #include <sys/stat.h>
-#include <cstdio>
-#include <iostream>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <cstring>
+#include <arpa/inet.h>
 #include <fstream>
 #include <sstream>
-#include <mysql/mysql.h>
 
 using namespace std;
 
-const int BUFSIZE = 10240;
-const int process = 10;
+void initDaemon();
 
-void initDaemon(){
-    setsid();
-    umask(0);
-    for(int i = 0; i < NOFILE; i++){
-        close(i);
-    }
-}
-
-int find(uint16_t vessel[process], uint16_t element){
-    int ans = -1;
-    for(int i = 0; i < process; i++){
-        if(element == vessel[i]) {
-            ans = i;
-            break;
-        }
-    }
-    return ans;
-}
-
-void createServer(int portNumber) {
-    int socketfd;
-    ofstream fout("main.pid.txt");
-    if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        fout << "Cannot create socket" << endl;
-        exit(0);
-    }
-
-    sockaddr_in servaddr;
-    sockaddr_in remaddr;
-    socklen_t addrlen = sizeof(servaddr);
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(uint16_t(portNumber));
-
-    if (bind(socketfd, (sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        fout << "Bind Failed" << endl;
-        exit(0);
-    }
-
-    int recvlen;
-    char buf[BUFSIZE];
-
-    uint16_t socketfdSets[process];
-    in_addr_t addrSets[process];
-    int getSocket = 0;
-    int finishSocket = 0;
-    memset(&socketfdSets, 0, sizeof(socketfdSets));
-    int socketProgress[process];
-    memset(&socketProgress, 0, sizeof(socketProgress));
-    uint32_t pidSets[process];
-    string timeSets[process];
-    string randomStrSets[process];
-
-    stringstream ss("");
-
-    for(;;){
-        recvlen = recvfrom(socketfd, buf, BUFSIZE, 0, (sockaddr*)&remaddr, &addrlen);
-        if(recvlen < 0) continue;
-        uint16_t port = remaddr.sin_port;
-        int found = find(socketfdSets, port);
-        fout << recvlen << endl;
-        if(found == -1){
-            //Can't find
-            socketfdSets[getSocket] = port;
-            addrSets[getSocket] = remaddr.sin_addr.s_addr;
-            found = getSocket;
-            getSocket += 1;
-        }
-        if(recvlen == 4){
-            uint32_t *pid = (uint32_t *)buf;
-            *pid = ntohl(*pid);
-            pidSets[found] = *pid;
-            fout << "PID: " << pidSets[found] << endl;
-        } else if (recvlen == 19){
-            buf[recvlen] = '\0';
-            timeSets[found] = buf;
-            fout << buf << endl;
-
-            //get random number between 5000 and 9999
-            srand(time(NULL) + getpid());
-            ss.str("");
-            ss << "str" << rand()%5000 + 5000;
-            sendto(socketfd, ss.str().c_str(), 7, 0, (sockaddr*) &remaddr, addrlen);
-        } else if (recvlen > 500){
-            buf[recvlen] = '\0';
-            randomStrSets[found] = buf;
-            fout << buf << endl;
-            finishSocket += 1;
-        }
-        fout << "FINISH" << endl;
-        if(finishSocket == process)
-            break;
-    }
-
-    for(int i = 0; i < process; i++){
-        memset(&remaddr, 0, sizeof(remaddr));
-        remaddr.sin_port = socketfdSets[i];
-        remaddr.sin_addr.s_addr = addrSets[i];
-        sendto(socketfd, "end", 3, 0, (sockaddr*) &remaddr, addrlen);
-    }
-
-    /*
-    for(int i = 0; i < process; i++){
-        MYSQL * mysql;
-        if((mysql = mysql_init(NULL)) == NULL){
-		    fout << "mysql_init failed" << endl;
-		    exit(-1);
-	    }
-	    mysql_set_character_set(mysql, "gbk");
-
-	    if(mysql_real_connect(mysql, "localhost", "root", "root123", "test", 0, NULL, 0) == NULL){
-		    fout << "mysql_real_connect failed(" << mysql_error(mysql) <<  ")" << endl;
-		    exit(-1);
-	    }
-
-	    ss.str("");
-	    ss << "insert into dbtest4 values (" << pidSets[i] << ", '" << timeSets[i] << "', '" << randomStrSets[i] << "')";
-	    cout << ss.str();
-	    if(mysql_query(mysql, ss.str().c_str())){
-		    fout << "mysql_query failed(" << mysql_error(mysql) << ")" << endl;
-	    }
-	    mysql_close(mysql);
-    }
-     */
-    close(socketfd);
-    fout << "EXIT" << endl;
-    exit(0);
-}
-
+void createClient(char* ipaddr, int PortNumber);
 
 int main(int argc, char * argv[]){
-    if(argc < 2){
-        cout << "Usage : test1-server <port_number> " <<endl;
+    if(argc < 3){
+        cout << "Usage: test1-client <IP_ADDR> <PORT_NUMBER>" << endl;
         exit(0);
     }
+
+    const int process = 10;
+
+    //GET IP
+    char * ipaddr = argv[1];
+
+    //GET PORT
     int portNumber;
-    portNumber = atoi(argv[1]);
+    portNumber = atoi(argv[2]);
+
     pid_t pid = fork();
     if(pid == -1){
         perror("fork new process error\n");
@@ -161,6 +39,117 @@ int main(int argc, char * argv[]){
     } else {
         //Child Process
         initDaemon();
-        createServer(portNumber);
+
+        for(int i = 0; i < process; i++) {
+            pid = fork();
+            if (pid < 0) {
+                perror("Fork Error");
+                exit(0);
+            } else if (pid > 0) {
+                continue;
+            } else if (pid == 0) {
+                createClient(ipaddr, portNumber);
+            }
+        }
     }
+}
+
+void initDaemon(){
+    setsid();
+    umask(0);
+    for(int i = 0; i < NOFILE; i++){
+        close(i);
+    }
+}
+
+void createClient(char* ipaddr, int PortNumber) {
+    int socketfd;
+    uint32_t mypid = getpid();
+    uint32_t mypid_network = htonl(mypid);
+    stringstream ss("");
+    ss << mypid << ".pid.txt";
+    ofstream fout(ss.str());
+    if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        fout << "Cannot create socket" << endl;
+        exit(0);
+    }
+
+    sockaddr_in myaddr;
+    sockaddr_in remaddr;
+    socklen_t slen = sizeof(myaddr);
+    memset(&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(0);
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(socketfd, (sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
+        fout << "bind failed" << endl;
+        exit(0);
+    }
+
+    memset(&remaddr, 0, sizeof(remaddr));
+    remaddr.sin_family = AF_INET;
+    remaddr.sin_port = htons(PortNumber);
+    if(inet_aton(ipaddr, &remaddr.sin_addr) == 0){
+        fout << "inet_aton() failed" << endl;
+        exit(1);
+    }
+
+    const int BUFSIZE = 10240;
+    char buf[BUFSIZE];
+
+    //Send PID
+    if(sendto(socketfd, &mypid_network, 4, 0, (sockaddr*)&remaddr, slen) == -1){
+        fout << "sendto failed" << endl;
+        exit(1);
+    }
+
+    //Get time & Send time
+    time_t now = time(0);
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    string time_str(buf);
+    if(sendto(socketfd, buf, 19, 0, (sockaddr*)&remaddr, slen) == -1){
+        fout << "sendto failed" << endl;
+        exit(1);
+    }
+
+    int recvlen;
+    int serverRand;
+
+    for(;;){
+        recvlen = recvfrom(socketfd, buf, BUFSIZE, 0, (sockaddr*)&remaddr, &slen);
+        if(recvlen <= 0) continue;
+        char serverRandom[5];
+        serverRandom[0] = buf[3];
+        serverRandom[1] = buf[4];
+        serverRandom[2] = buf[5];
+        serverRandom[3] = buf[6];
+        serverRandom[4] = '\0';
+        serverRand = atoi(serverRandom);
+        break;
+    }
+
+    const char * charPool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const int charNum = 52;
+    srand(time(NULL) + mypid);
+    for(int i = 0; i < serverRand; i++){ buf[i] = charPool[rand()%charNum]; }
+    buf[serverRand] = '\0';
+
+    string randomStr(buf);
+
+    if(sendto(socketfd, buf, strlen(buf), 0, (sockaddr*)&remaddr, slen) == -1){
+        fout << "sendto failed" << endl;
+        exit(1);
+    }
+
+    for(;;){
+        recvlen = recvfrom(socketfd, buf, BUFSIZE, 0, (sockaddr*)&remaddr, &slen);
+        if(recvlen <= 0) continue;
+        break;
+    }
+
+    fout << mypid << endl << time_str << endl << randomStr << flush;
+    close(socketfd);
+
+    exit(0);
 }
