9d32ea84eb18fc5d567781a0545d763103f33d93
LovingYoung <ly950710@gmail.com>
2016-04-25 20:05:21 +0800

    Update W0802-test4-server.cpp

--- W0802-test4-server.cpp
+++ W0802-test4-server.cpp
@@ -6,8 +6,13 @@
 #include <netinet/in.h>
 #include <cstring>
 #include <fstream>
+#include <sstream>
+#include <mysql/mysql.h>
 
 using namespace std;
+
+const int BUFSIZE = 10240;
+const int process = 10;
 
 void initDaemon(){
     setsid();
@@ -17,10 +22,22 @@ void initDaemon(){
     }
 }
 
+int find(uint16_t vessel[process], uint16_t element){
+    int ans = -1;
+    for(int i = 0; i < process; i++){
+        if(element == vessel[i]) {
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+}
+
 void createServer(int portNumber) {
     int socketfd;
-    if((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-        perror("Cannot create socket");
+    ofstream fout("main.pid.txt");
+    if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        fout << "Cannot create socket" << endl;
         exit(0);
     }
 
@@ -32,24 +49,101 @@ void createServer(int portNumber) {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(uint16_t(portNumber));
 
-    if(bind(socketfd, (sockaddr*)&servaddr, sizeof(servaddr)) < 0){
-        perror("Bind Failed");
+    if (bind(socketfd, (sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        fout << "Bind Failed" << endl;
         exit(0);
     }
 
-    const int BUFSIZE = 10240;
     int recvlen;
     char buf[BUFSIZE];
-    ofstream fout("main.pid.txt");
+
+    uint16_t socketfdSets[process];
+    in_addr_t addrSets[process];
+    int getSocket = 0;
+    int finishSocket = 0;
+    memset(&socketfdSets, 0, sizeof(socketfdSets));
+    int socketProgress[process];
+    memset(&socketProgress, 0, sizeof(socketProgress));
+    uint32_t pidSets[process];
+    string timeSets[process];
+    string randomStrSets[process];
+
+    stringstream ss("");
 
     for(;;){
         recvlen = recvfrom(socketfd, buf, BUFSIZE, 0, (sockaddr*)&remaddr, &addrlen);
-        if(recvlen > 0){
-            buf[recvlen] = '\0';
-            fout << buf << endl;
+        if(recvlen < 0) continue;
+        uint16_t port = remaddr.sin_port;
+        int found = find(socketfdSets, port);
+        fout << recvlen << endl;
+        if(found == -1){
+            //Can't find
+            socketfdSets[getSocket] = port;
+            addrSets[getSocket] = remaddr.sin_addr.s_addr;
+            found = getSocket;
+            getSocket += 1;
         }
+        if(recvlen == 4){
+            uint32_t *pid = (uint32_t *)buf;
+            *pid = ntohl(*pid);
+            pidSets[found] = *pid;
+            fout << "PID: " << pidSets[found] << endl;
+        } else if (recvlen == 19){
+            buf[recvlen] = '\0';
+            timeSets[found] = buf;
+            fout << buf << endl;
+
+            //get random number between 5000 and 9999
+            srand(time(NULL) + getpid());
+            ss.str("");
+            ss << "str" << rand()%5000 + 5000;
+            sendto(socketfd, ss.str().c_str(), 7, 0, (sockaddr*) &remaddr, addrlen);
+        } else if (recvlen > 500){
+            buf[recvlen] = '\0';
+            randomStrSets[found] = buf;
+            fout << buf << endl;
+            finishSocket += 1;
+        }
+        fout << "FINISH" << endl;
+        if(finishSocket == process)
+            break;
     }
+
+    for(int i = 0; i < process; i++){
+        memset(&remaddr, 0, sizeof(remaddr));
+        remaddr.sin_port = socketfdSets[i];
+        remaddr.sin_addr.s_addr = addrSets[i];
+        sendto(socketfd, "end", 3, 0, (sockaddr*) &remaddr, addrlen);
+    }
+
+    /*
+    for(int i = 0; i < process; i++){
+        MYSQL * mysql;
+        if((mysql = mysql_init(NULL)) == NULL){
+		    fout << "mysql_init failed" << endl;
+		    exit(-1);
+	    }
+	    mysql_set_character_set(mysql, "gbk");
+
+	    if(mysql_real_connect(mysql, "localhost", "root", "root123", "test", 0, NULL, 0) == NULL){
+		    fout << "mysql_real_connect failed(" << mysql_error(mysql) <<  ")" << endl;
+		    exit(-1);
+	    }
+
+	    ss.str("");
+	    ss << "insert into dbtest4 values (" << pidSets[i] << ", '" << timeSets[i] << "', '" << randomStrSets[i] << "')";
+	    cout << ss.str();
+	    if(mysql_query(mysql, ss.str().c_str())){
+		    fout << "mysql_query failed(" << mysql_error(mysql) << ")" << endl;
+	    }
+	    mysql_close(mysql);
+    }
+     */
+    close(socketfd);
+    fout << "EXIT" << endl;
+    exit(0);
 }
+
 
 int main(int argc, char * argv[]){
     if(argc < 2){
