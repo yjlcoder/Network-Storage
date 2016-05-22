aa433891f49805289c4b54795fc18dc950a67f70
LovingYoung <ly950710@gmail.com>
2016-05-22 19:22:21 +0800

    Create File table

--- Webpages/Driver/app/models.py
+++ Webpages/Driver/app/models.py
@@ -3,7 +3,8 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(128), index=True, unique=True, nullable=False)
-    password = db.Column(db.String(40), index=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    files = db.relationship('File', backref="uploader", lazy='dynamic')
 
     def __repr__(self):
         return "<User %r>" % (self.username)
@@ -22,3 +23,11 @@ class User(db.Model):
 
     def get_id(self):
         return str(self.id)
+
+class File(db.Model):
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    virtualdir = db.Column(db.String(190), primary_key=True)
+    md5 = db.Column(db.CHAR(32), nullable=False)
+
+    def __repr__(self):
+        return "<UserID: %r, Virtual Directory: %r>" %(self.userid, self.virtualDir)
--- Webpages/Driver/app/views.py
+++ Webpages/Driver/app/views.py
@@ -1,6 +1,6 @@
 from flask.ext.login import login_user, login_required, logout_user, current_user
 from app import app, forms, db, models, login_manager
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 import hashlib
 
 
@@ -8,22 +8,30 @@ import hashlib
 @app.route("/index")
 @login_required
 def index():
-    return render_template("index.html")
+    path = request.args.get('path')
+    if path is None or path == "":
+        path = '/'
+        return redirect(url_for('index', path=path))
+    #TODO: get data from database
+    return render_template("index.html", path = path)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user is not None and current_user.is_authenticated:
-        flash("您已经登陆，请注销后重试")
-        redirect('index')
+        flash("您已经登录，请注销后重试")
+        return redirect('index')
     form = forms.LoginForm(request.form)
     if request.method == "POST" and form.validate():
         user = models.User.query.filter_by(username=form.username.data).first()
-        if(user.password != hashlib.sha1(form.password.data.encode('utf-8')).hexdigest()):
-            flash("登陆失败, 密码错误")
+        if user is None:
+            flash("不存在该用户名")
+            return redirect('login')
+        if user.password != hashlib.sha1(form.password.data.encode('utf-8')).hexdigest():
+            flash("登录失败, 密码错误")
             return redirect('login')
         login_user(user, remember=form.remember_me.data)
-        flash("登陆成功")
+        flash("登录成功")
         next = request.args.get('next')
         if next is None or next == "":
             return redirect('index')
@@ -42,7 +50,10 @@ def register():
             flash("该用户名已经被注册")
             return redirect('register')
         user = models.User(username=form.username.data, password=hashlib.sha1(form.password.data.encode('utf-8')).hexdigest())
+        file = models.File(virtualdir='/', md5="00000000000000000000000000000000", uploader=user)
         db.session.add(user)
+        db.session.commit()
+        db.session.add(file)
         db.session.commit()
         flash("注册成功")
         return redirect('login')
