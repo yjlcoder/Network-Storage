881be1fb5c193885babec14b56289cb71565803c
LovingYoung <ly950710@gmail.com>
2016-05-22 11:18:52 +0800

    Webpages: import bootstrap & add navbar

---
+++ Webpages/Driver/app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
