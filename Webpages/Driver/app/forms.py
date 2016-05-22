00b295b998b07b90c5e1c1949543578bd9dcd139
LovingYoung <ly950710@gmail.com>
2016-05-22 15:37:47 +0800

    raise Error when input username include characters

--- Webpages/Driver/app/forms.py
+++ Webpages/Driver/app/forms.py
@@ -1,10 +1,15 @@
 from app import app
 from wtforms import Form, BooleanField, StringField, PasswordField
-from wtforms import validators
+from wtforms import validators, ValidationError
 
+def usernameCheck(form, field):
+    valid = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    for i in field.data:
+        if i not in valid:
+            raise ValidationError("用户名只能是数字或字母")
 
 class RegisterForm(Form):
-    username = StringField('Username', [validators.DataRequired(message="请输入用户名"), validators.Length(min=5, max=15, message="用户名长度不符合要求，请输入5-15位用户名")])
+    username = StringField('Username', [validators.DataRequired(message="请输入用户名"), validators.Length(min=5, max=15, message="用户名长度不符合要求，请输入5-15位用户名"), usernameCheck])
     password = PasswordField('Password', [validators.DataRequired(message="请输入密码"), validators.Length(min=5, max=15, message="密码长度不符合要求, 请输入5-15位密码")])
     confirm = PasswordField('Confirm', [validators.DataRequired(message="请再次输入密码"), validators.equal_to('password', message='两次密码不匹配')])
 
@@ -13,7 +18,7 @@ class RegisterForm(Form):
 
 
 class LoginForm(Form):
-    username = StringField('Username', [validators.DataRequired(message="请输入用户名"), validators.Length(min=5, max=15, message="用户名长度不符合要求，请输入5-15位用户名")])
+    username = StringField('Username', [validators.DataRequired(message="请输入用户名"), validators.Length(min=5, max=15, message="用户名长度不符合要求，请输入5-15位用户名"), usernameCheck])
     password = PasswordField('Password', [validators.DataRequired(message="请输入密码"), validators.Length(min=5, max=15, message="密码长度不符合要求, 请输入5-15位密码")])
     remember_me = BooleanField("Remember Me")
 
